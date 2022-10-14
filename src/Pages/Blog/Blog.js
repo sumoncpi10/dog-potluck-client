1a21198b20e4004e2d6e1d8f79b4114d4d91b919
sumon <sumoncpi10@gmail.com>
2022-10-14 23:11:30 +0600

    ok

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -3,25 +3,27 @@ import { Link, useNavigate } from 'react-router-dom';
 
 const Blog = ({ blog }) => {
     const navigate = useNavigate();
-    // console.log(blog);
+    console.log(blog);
     return (
 
         <div className="col-md-6 col-lg-4">
 
             <div className="post-item">
                 <div className="thumb">
-                    <a href="blog-details.html">
+                    <Link to={`/blog/${blog?._id}`}>
                         <img src={blog?.img || blog?.img1} width="350" height="250" alt="Image-HasTech" />
-                    </a>
+                    </Link>
                 </div>
                 <div className="content">
                     <div className="meta">
                         <ul>
-                            <li className="author-info"><span>By:</span> <a href="blog.html">Admin</a></li>
-                            <li className="post-date"><a href="blog.html">{blog?.date}</a></li>
+                            <li className="author-info"><span>By:</span> <a href="/blog">Admin</a></li>
+                            {/* <li className="post-date"><a href="/blog">{blog?.date}</a></li> */}
+
                         </ul>
                     </div>
-                    <h4 className="title"><a href="blog-details.html">{blog?.Tittle1}</a></h4>
+                    <h4 className="title"><Link to={`/blog/${blog?._id}`}>{blog?.Tittle1}</Link></h4>
+                    {/* <p>{blog?.description11}</p> */}
                     <Link className="btn-theme btn-sm" to={`/blog/${blog?._id}`}>Read More</Link>
                 </div>
             </div>
